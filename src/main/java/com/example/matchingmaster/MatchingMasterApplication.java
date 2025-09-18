318dd681448015dd4f97b528410777e05a694804
박하민 <hamin090403@gmail.com>
2025-09-18 20:28:16 +0900

    init

---
+++ src/main/java/com/example/matchingmaster/MatchingMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.matchingmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchingMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/matchingmaster/MatchingMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matchingmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
